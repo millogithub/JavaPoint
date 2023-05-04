6ea18c45c73181b60dbdd68dce2bdebe2d91f653
Menelik <millo.sharepoint@gmail.com>
2023-05-04 12:47:11 -0400

    Modify

--- cJava Object Class/new 1.java
+++
@@ -1,9 +0,0 @@
-//user defined method  
-public static void findEvenOdd(int num)  
-{  
-//method body  
-if(num%2==0)   
-System.out.println(num+" is even");   
-else   
-System.out.println(num+" is odd");  
-}  
